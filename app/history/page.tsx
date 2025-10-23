e4c504585535f06654ee09d4d3891cda88c08c25
George Wallace <george.wallace@elastic.co>
2025-10-23 09:35:54 -0600

    fixes

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -149,7 +149,7 @@ export default async function ShooterHistoryPage() {
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
-                  {shootsWithTotals.map((shoot) => (
+                  {shootsWithTotals.map((shoot: any) => (
                     <tr key={shoot.id} className="hover:bg-gray-50">
                       <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
                         {format(new Date(shoot.date), 'PP')}
