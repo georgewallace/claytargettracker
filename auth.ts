4b20b93a94544460d8289f2de981ab06eeb76067
George Wallace <george.wallace@elastic.co>
2025-10-25 19:47:01 -0600

    Fix: Explicitly pass AUTH_SECRET to NextAuth config
    - Auth.js v5 requires secret to be explicitly defined in config
    - Fixes MissingSecret error even when AUTH_SECRET env var is set
    - Resolves login errors on staging

--- auth.ts
+++ auth.ts
@@ -6,6 +6,7 @@ import bcrypt from 'bcrypt'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  secret: process.env.AUTH_SECRET,
   providers: [
     Credentials({
       name: 'credentials',
